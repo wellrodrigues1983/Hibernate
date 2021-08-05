6fbdcb315cfc31a93d1a4f00832ac9511eeef228
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-08-05 15:19:54 -0300

    Projeto configurado

---
+++ src/main/java/mavemhibernate/HibernateUtil.java
@@ -0,0 +1,32 @@
+package mavemhibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+	
+	public static EntityManagerFactory factory = null;
+	
+	static {
+		init();
+	}
+	
+	private static void init() {
+		
+		try {
+			if (factory == null) {
+				factory = Persistence.createEntityManagerFactory("mavem-hibernate");
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		
+	}
+
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager(); //prove a parte de persistencia
+	}
+}
---
+++ src/test/java/mavemhibernate/TesteHibernate.java
@@ -0,0 +1,12 @@
+package mavemhibernate;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+	
+	@Test
+	public void testeHibernate() {
+		HibernateUtil.getEntityManager();
+	}
+
+}

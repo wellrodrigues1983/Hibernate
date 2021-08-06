448527788eb4c502a461b18defcc1341ec523930
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-08-05 21:40:26 -0300

    Criado DaoGeneric Update

--- src/main/java/dao/DaoGeneric.java
+++ src/main/java/dao/DaoGeneric.java
@@ -17,13 +17,28 @@ public class DaoGeneric<E> {
 	}
 	
 	public E pesquisar(E entidade) {
-		
+
 		Object id = HibernateUtil.getPrimaryKey(entidade);
-		
+
 		E e = (E) entityManager.find(entidade.getClass(), id);
-		
+
 		return e;
+
+	}	
+	
+	public E pesquisar(Long id, Class<E> entidade) {
+		E e = (E) entityManager.find(entidade, id);
+		return e;
+
+	}
+	
+	public E updateMerge(E entidade) { //Salva ou atualiza
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		E entidadeSalva = entityManager.merge(entidade);
+		transaction.commit();
+		return entidadeSalva;
 	}
 
-
+	
 }
--- src/test/java/mavemhibernate/TesteHibernate.java
+++ src/test/java/mavemhibernate/TesteHibernate.java
@@ -7,32 +7,48 @@ import model.UsuarioPessoa;
 
 public class TesteHibernate {
 	
-	@Test
-	public void testeHibernate() {
-		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
-		
-		UsuarioPessoa pessoa = new UsuarioPessoa();
-		
-		pessoa.setNome("Wellington");
-		pessoa.setSobrenome("Rodrigues");
-		pessoa.setEmail("wellrodrigues@gmail.com");
-		pessoa.setLogin("wellrodrigues");
-		pessoa.setSenha("123");
-		
-		daoGeneric.salvar(pessoa);
-	}
+//	@Test
+//	public void testeHibernate() {
+//		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+//		
+//		UsuarioPessoa pessoa = new UsuarioPessoa();
+//		
+//		pessoa.setNome("Wellington");
+//		pessoa.setSobrenome("Rodrigues");
+//		pessoa.setEmail("wellrodrigues@gmail.com");
+//		pessoa.setLogin("wellrodrigues");
+//		pessoa.setSenha("123");
+//		
+//		daoGeneric.salvar(pessoa);
+//	}
+	
+//	@Test
+//	public void testeBuscar() {
+//		DaoGeneric<UsuarioPessoa> daoGeneric= new DaoGeneric<UsuarioPessoa>();
+//		
+//		UsuarioPessoa pessoa = new UsuarioPessoa();
+//		pessoa.setId(2L);
+//		
+//		pessoa = daoGeneric.pesquisar(pessoa);
+//		
+//		System.out.println(pessoa);
+//		
+//		
+//	}
 	
 	@Test
-	public void testeBuscar() {
-		DaoGeneric<UsuarioPessoa> daoGeneric= new DaoGeneric<UsuarioPessoa>();
+	public void testeUpdate(){
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
 		
-		UsuarioPessoa pessoa = new UsuarioPessoa();
-		pessoa.setId(2L);
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(1L , UsuarioPessoa.class);
 		
-		pessoa = daoGeneric.pesquisar(pessoa);
+		
+		pessoa.setNome("Nome atualizado Hibernate");
+		pessoa.setSenha("sd4s5d4s4d");
+		
+		pessoa = daoGeneric.updateMerge(pessoa);
 		
 		System.out.println(pessoa);
-		
 		
 	}
 
